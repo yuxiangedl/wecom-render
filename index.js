de5a59cc925c9a7e909a592c49f14d6f415f840d
yuxiangedl <yuxiang.wang@evolutiondesignlab.com>
2025-03-26 12:31:03 -0700

    redp-2

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get("/receive", (req, res) => {
   res.send(echostr || "hello");
 });
 
-app.listen(port, () => {
-  console.log(`✅ Server running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`✅ Server running on http://0.0.0.0:${port}`);
 });
